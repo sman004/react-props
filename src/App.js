b6931fa56e9d6b81f7c39457479fc28f7ed846c1
sman004 <sparklins4u@gmail.com>
2023-09-26 14:09:28 +0100

    React-intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./components/Card";
+import contacts from "./components/contact";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card
+       name = {contacts[0].name}
+       img = {contacts[0].imgURL}
+       tel = {contacts[0].phone}
+       email={contacts[0].email}
+      />
+
+      <Card
+        name = {contacts[1].name}
+        img = {contacts[1].imgURL}
+        tel = {contacts[1].phone}
+        email={contacts[1].email}
+      />
+
+<Card
+        name = {contacts[2].name}
+        img = {contacts[2].imgURL}
+        tel = {contacts[2].phone}
+        email={contacts[2].email}
+      />
+     
     </div>
   );
 }
